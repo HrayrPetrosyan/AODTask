42ea09654658d02793ba622e0cc9202798d693cd
Hrayr Petrosyan <44923866+HrayrPetrosyan@users.noreply.github.com>
2019-07-12 18:41:59 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const regionRoutes = require('./routes/region');
+const countryRoutes = require('./routes/country');
+const cityRoutes = require('./routes/city');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  next()
+})
+
+app.use('/region', regionRoutes);
+app.use('/country', countryRoutes);
+app.use('/city', cityRoutes);
+
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect('mongodb+srv://AngUser:X8HC2SMzIgwgGkHg@cluster0-p1nio.mongodb.net/test?retryWrites=true', {useNewUrlParser: true, useFindAndModify: false})
+  .then(result => {
+    app.listen("3000");
+  })
+  .catch(err => {
+    console.log(err)
+  })
